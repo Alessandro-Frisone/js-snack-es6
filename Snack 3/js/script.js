06aee5803df69805243213ba1cd0aae3df1fef8f
Alessandro-Frisone <alefri2004@gmail.com>
2024-12-12 16:31:14 +0100

    feat : last Snack

---
+++ Snack 3/js/script.js
@@ -0,0 +1,12 @@
+// Funzione che accetti trew argometni (array e due numeri)
+function filter(array, a, b) {
+    let secondArray = []
+    for (let i = 0; i < array.lenght; i++) {
+        if (a <= i && i <= b) {
+            secondArray.push(array[i])
+        }
+    }
+    return secondArray;
+}
+const finalArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(filter(finalArray, 1, 8))
